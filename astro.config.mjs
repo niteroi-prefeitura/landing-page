8596a4926f1d42ee011b8a5e46092aef1c5e07fb
DaniloRCBGG <danilorios67@gmail.com>
2025-10-16 15:40:56 -0300

    configuração do astro e tailwind com o tema usado no gemeo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    tailwind({
+      applyBaseStyles: true, // aplica estilos padrões do Tailwind
+    }),
+  ],
+});
---
+++ tailwind.config.cjs
@@ -0,0 +1,59 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}"],
+  theme: {
+    extend: {
+      colors: {
+        text: "#ffffff",
+        textMuted: "#b4b4b4",
+
+        bus: {
+          normal: {
+            DEFAULT: "#336a16",
+            accent: "#559f06",
+          },
+          alert: {
+            DEFAULT: "rgba(229, 160, 0, 0.8)",
+            accent: "#ffaa00",
+          },
+          jammed: {
+            DEFAULT: "#b4631b",
+            accent: "#ff7700",
+          },
+          alert_jammed: {
+            DEFAULT: "#b4631b",
+            accent: "#ff7700",
+          },
+        },
+
+        highlight: {
+          cyan: "#00d2f0",
+        },
+
+        background: {
+          navy: "#001a2d",
+          navyAccent: "#004064",
+          button: "#002b4a",
+          buttonHover: "#02365bff",
+          gray: "#9f9898",
+          progress: "#145e94",
+          landing: "#08051A",
+          landingThemes: "rgba(255, 255, 255, 0.05)",
+          landingButton: "rgba(255, 255, 255, 0.1)",
+          landingButtonHover: "rgba(255, 255, 255, 0.3)",
+        },
+
+        camera: {
+          DEFAULT: "#8583E3",
+          accent: "#A29BFE",
+        },
+      },
+      fontFamily: {
+        primary: ['"IBM Plex Sans"', "sans-serif"],
+        secondary: ['"Oxygen"', "sans-serif"],
+        alt: ['"Orbitron"', "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};

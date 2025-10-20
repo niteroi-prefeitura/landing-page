5e2b9cf7cc6ef451b7f5ba0983f40ce4f44d6560
DaniloRCBGG <danilorios67@gmail.com>
2025-10-20 17:58:54 -0300

    Adicionando react as ependdencias afim de ajustar o visual da pagina

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: true, // aplica estilos padrões do Tailwind
-    }),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: true, // aplica estilos padrões do Tailwind
+  }), react()],
 });

7a26b43a2c64cf7ef02f56b3033cae57b34a03c6
DaniloRCBGG <danilorios67@gmail.com>
2025-10-21 15:17:58 -0300

    chore: Removendo react das dependencias e excluindo component react

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   integrations: [
     tailwind({
-      applyBaseStyles: true, // aplica estilos padrões do Tailwind
+      applyBaseStyles: true,
     }),
   ],
 });
